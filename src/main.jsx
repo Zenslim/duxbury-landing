05ff550533cacaaad597f89e6472a16ef35f037e
Zenslim <zenslim2010@gmail.com>
2025-04-08 14:49:47 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DuxburyLanding from "./components/DuxburyLanding";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <DuxburyLanding />
-  </React.StrictMode>
-);
+import Navbar from "./components/Navbar";
+import { Hero } from './components/Hero';
+import { About } from './components/About';
+import { Programs } from './components/Programs';
+import { Vision } from './components/Vision';
+import { BlogTeaser } from './components/BlogTeaser';
+import { Duxbury } from './components/Duxbury';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
+function Main() {
+  return (
+    <main className="text-gray-800 bg-white scroll-smooth">
+      <Navbar />
