87c91ee6b55eca2d9fd6ecc81fc104bd4110c48a
Zenslim <zenslim2010@gmail.com>
2025-04-08 11:12:01 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomePage from "./HomePage";
+import DuxburyLanding from "./components/DuxburyLanding";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
+    <DuxburyLanding />
   </React.StrictMode>
 );
